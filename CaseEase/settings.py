123a3e38d263e5bf2337c9eaf78ee1f6f3fad3a6
duaamir-azm <dua.amir@azm.dev>
2025-07-29 10:13:26 +0500

    changes to deploy on render

--- CaseEase/settings.py
+++ CaseEase/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2k*y!ni@i@0ai7q53^lyj3wm@0yn*4vk$blp@v^h4@!+ir4yk&'
+import os
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -100,16 +101,23 @@ WSGI_APPLICATION = 'CaseEase.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'caseease_db',
+#         'USER': 'caseease_user',
+#         'PASSWORD': 'mypassword123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'caseease_db',
-        'USER': 'caseease_user',
-        'PASSWORD': 'mypassword123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 
 # Password validation
